38ece32a2d585aa30f47952324069e614c9cae8c
Dominic Chu <dominic.m.chu@gmail.com>
2020-02-27 11:00:29 -0500

    Done.

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,8 +1,21 @@
 export default function manageTodo(state = {
-  todos: [],
+  todos: [
+    {}
+  ],
 }, action) {
 
-  console.log("reducer received this action:", action);
+  switch (action.type) {
+    case 'ADD_TODO': 
+      console.log({
+        todos: 
+        state.todos.concat(action.payload.text)
+      })
+      return {
+        todos: state.todos.concat(action.payload.text)
+      }
+    default: 
+      return state; 
+  }
 
   return state;
 }
